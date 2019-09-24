59f505316c00422304c539a85bc40a3256beb8ff
lukemcd9 <34250936+lukemcd9@users.noreply.github.com>
2019-09-24 13:55:35 -1000

    Quartz jobs now extend SubmitJob

--- src/main/java/edu/hawaii/its/filedrop/job/ExpireJob.java
+++ src/main/java/edu/hawaii/its/filedrop/job/ExpireJob.java
@@ -1,13 +1,12 @@
 package edu.hawaii.its.filedrop.job;
 
-import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.hawaii.its.filedrop.service.FileDropService;
 
-public class ExpireJob implements Job {
+public class ExpireJob extends SubmitJob {
 
     @Autowired
     private FileDropService fileDropService;
