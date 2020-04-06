f021256fd1d754a12bc7a418c6e181a0da20493b
lukemcd9 <34250936+lukemcd9@users.noreply.github.com>
2020-04-06 09:27:01 -1000

    Fix tests

--- src/main/java/edu/hawaii/its/filedrop/configuration/AppConfigLocal.java
+++ src/main/java/edu/hawaii/its/filedrop/configuration/AppConfigLocal.java
@@ -4,23 +4,37 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.ldap.core.support.LdapContextSource;
 
-@Profile(value = { "default", "dev" })
+@Profile(value = { "localhost" })
 @Configuration
 @ComponentScan(basePackages = "edu.hawaii.its.filedrop")
 @EnableJpaRepositories(basePackages = { "edu.hawaii.its.filedrop.repository" })
-@PropertySource("classpath:custom.properties")
-public class AppConfig {
+@PropertySources({
+        @PropertySource("classpath:custom.properties"),
+        @PropertySource(value = "file:${user.home}/.${user.name}-conf/filedrop-overrides.properties",
+                ignoreResourceNotFound = true)
+})
+public class AppConfigLocal {
 
     private static final Log logger = LogFactory.getLog(AppConfig.class);
 
     @PostConstruct
     public void init() {
         logger.info("AppConfig init");
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "app.ldap.context-source")
+    public LdapContextSource ldapContextSource() {
+        return new LdapContextSource();
     }
 }
