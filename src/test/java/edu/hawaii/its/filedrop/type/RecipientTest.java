bd37049859cfd0f6a45bf43ff92dddf6f455c6ec
Frank Duckart <duckart@computer.org>
2020-04-05 22:31:13 -1000

    New tests.

---
+++ src/test/java/edu/hawaii/its/filedrop/type/RecipientTest.java
@@ -0,0 +1,57 @@
+package edu.hawaii.its.filedrop.type;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecipientTest {
+
+    Recipient recipient;
+
+    @Before
+    public void setUp() {
+        recipient = new Recipient();
+    }
+
+    @Test
+    public void accessors() {
+        assertThat(recipient.getId(), equalTo(null));
+        assertThat(recipient.getName(), equalTo(null));
+        assertThat(recipient.getFileDrop(), equalTo(null));
+
+        recipient.setId(666);
+        recipient.setName("TB");
+        assertThat(recipient.getId(), equalTo(666));
+        assertThat(recipient.getName(), equalTo("TB"));
+
+        FileDrop fileDrop = new FileDrop();
+        fileDrop.setId(123);
+        recipient.setFileDrop(fileDrop);
+        assertThat(recipient.getFileDrop(), not(equalTo(null)));
+        assertThat(recipient.getFileDrop().getId(), equalTo(123));
+    }
+
+    @Test
+    public void testToString() {
+        assertThat(recipient.toString(), startsWith("Recipient ["));
+        assertThat(recipient.toString(), containsString("id=null, "));
+        assertThat(recipient.toString(), containsString(", name=null"));
+        assertThat(recipient.toString(), containsString(", fileDrop.id=null"));
+
+        recipient.setId(666);
+        assertThat(recipient.toString(), startsWith("Recipient [id=666"));
+
+        recipient.setName("EX");
+        assertThat(recipient.toString(), containsString(", name=EX"));
+
+        FileDrop fileDrop = new FileDrop();
+        fileDrop.setId(321);
+        recipient.setFileDrop(fileDrop);
+        assertThat(recipient.toString(), containsString(", fileDrop.id=321"));
+    }
+}
