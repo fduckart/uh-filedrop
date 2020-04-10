2956da9aff60745ef4dee4fd0d4efaa71f8456cf
Frank Duckart <duckart@computer.org>
2020-04-09 19:08:36 -1000

    New converter class.

---
+++ src/main/java/edu/hawaii/its/filedrop/type/BooleanToCharacterConverter.java
@@ -0,0 +1,22 @@
+package edu.hawaii.its.filedrop.type;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BooleanToCharacterConverter implements AttributeConverter<Boolean, Character> {
+
+    private static final Character YES = Character.valueOf('Y');
+    private static final Character NO = Character.valueOf('N');
+
+    @Override
+    public Character convertToDatabaseColumn(Boolean value) {
+        return (value != null && value) ? YES : NO;
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(Character value) {
+        return YES.equals(value);
+    }
+
+}
---
+++ src/test/java/edu/hawaii/its/filedrop/type/BooleanToCharacterConverterTest.java
@@ -0,0 +1,42 @@
+package edu.hawaii.its.filedrop.type;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BooleanToCharacterConverterTest {
+
+    private BooleanToCharacterConverter converter;
+
+    private final Character YES = Character.valueOf('Y');
+    private final Character NO = Character.valueOf('N');
+
+    @Before
+    public void setUp() {
+        converter = new BooleanToCharacterConverter();
+    }
+
+    @Test
+    public void convertToDatabaseColumn() {
+        assertThat(converter.convertToDatabaseColumn(Boolean.TRUE), equalTo(YES));
+        assertThat(converter.convertToDatabaseColumn(TRUE), equalTo(YES));
+        assertThat(converter.convertToDatabaseColumn(FALSE), equalTo(NO));
+        assertThat(converter.convertToDatabaseColumn(null), equalTo(NO));
+    }
+
+    @Test
+    public void convertToEntityAttribute() {
+        assertThat(converter.convertToEntityAttribute(YES), equalTo(TRUE));
+        assertThat(converter.convertToEntityAttribute(NO), equalTo(FALSE));
+
+        // Unexpected cases.
+        assertThat(converter.convertToEntityAttribute(null), equalTo(FALSE));
+        assertThat(converter.convertToEntityAttribute(Character.valueOf('T')),
+                equalTo(FALSE));
+    }
+
+}
