3c4058521d0cc7cb38e2e317254ee990dffb5b49
Frank Duckart <duckart@computer.org>
2020-08-20 19:41:47 -1000

    Metamodel tweaks.

--- src/main/java/edu/hawaii/its/filedrop/type/FileSet_.java
+++ src/main/java/edu/hawaii/its/filedrop/type/FileSet_.java
@@ -5,7 +5,11 @@ import javax.persistence.metamodel.SingularAttribute;
 public class FileSet_ {
 
     public static volatile SingularAttribute<FileSet, Integer> id;
+    public static volatile SingularAttribute<FileSet, String> comment;
     public static volatile SingularAttribute<FileSet, FileDrop> fileDrop;
+    public static volatile SingularAttribute<FileSet, String> fileName;
+    public static volatile SingularAttribute<FileSet, Long> size;
+    public static volatile SingularAttribute<FileSet, String> type;
 
     // Private contructor to prevent instantiation.
     private FileSet_() {
