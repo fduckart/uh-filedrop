bc9b2a527a58362267da0f4442c60915fd7beae5
lukemcd9 <34250936+lukemcd9@users.noreply.github.com>
2020-09-08 13:43:40 -1000

    Remove file_data reference

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE fd_fileset (
   size BIGINT NOT NULL DEFAULT '0',
   id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
-  CONSTRAINT file_data_ibfk_1 FOREIGN KEY (filedrop_id) REFERENCES fd_filedrop (id) ON DELETE CASCADE
+  CONSTRAINT fileset_ibfk_1 FOREIGN KEY (filedrop_id) REFERENCES fd_filedrop (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS recipient;
