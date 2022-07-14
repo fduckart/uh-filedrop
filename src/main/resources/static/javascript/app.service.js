7a887cac08e2051157793d3f2e119f771d848104
Frank Duckart <duckart@computer.org>
2022-07-13 18:46:34 -1000

    wip

--- src/main/resources/static/javascript/app.service.js
+++ src/main/resources/static/javascript/app.service.js
@@ -1,19 +1,27 @@
-(function () {
+(function() {
 
-    filedropApp.factory("dataProvider", function ($http, $log) {
+    filedropApp.factory("whatsUp", function() {
+        return {
+            testme: function() {
+                // Do nothing.
+            }
+        };
+    });
+
+    filedropApp.factory("dataProvider", function($http, $log) {
         return {
             loadData: function loadData(callback, url) {
-                $http.get(encodeURI(url)).then(callback, function (response, status) {
+                $http.get(encodeURI(url)).then(callback, function(response, status) {
                     $log.error("Error in dataProvider; status: ", status);
                 });
             },
             delData: function delData(callback, url) {
-                $http.delete(encodeURI(url)).then(callback, function (data, status) {
+                $http.delete(encodeURI(url)).then(callback, function(data, status) {
                     $log.error("Error in dataProvider; status: ", status);
                 });
             },
             saveData: function saveData(callback, url, data) {
-                $http.post(encodeURI(url), data).then(callback, function (response, status) {
+                $http.post(encodeURI(url), data).then(callback, function(response, status) {
                     $log.error("Error in dataProvider; status: ", status);
                 });
             }
